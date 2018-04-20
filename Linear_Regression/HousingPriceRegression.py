949ba1a0819db042d69aa869fec98271a62e7f1b
Eric-D-Stevens <ericstevens26101@gmail.com>
2018-04-20 16:41:42 -0700

    finish cleaning and output

--- Linear_Regression/HousingPriceRegression.py
+++ Linear_Regression/HousingPriceRegression.py
@@ -1,3 +1,6 @@
+'''Use Python 2.7: Simple linear regression using the Boston Housing
+dataset from Carnegie Mellon. The data and the infomation about the
+data can be found at http://lib.stat.cmu.edu/datasets/boston '''
 
 import numpy as np
 import random as rand
@@ -15,7 +18,7 @@ def load_matrix(filename):
 	Y = X.copy()
 	Y = Y[:,13]
 
-	# place one
+	# place ones dummy variable where Y was
 	X[:,13] = 1
 
 	# return the matricies
@@ -63,8 +66,8 @@ def quick_sse(X,Y):
 	return [W,E]
 
 # Adds 'numOfFeats' random features incrementally and records
-# the effect on treaining and testing data performance to 
-# demonstrate the result of overfitting in a case were non causal 
+# the effect on treaining and testing data performance to
+# demonstrate the result of overfitting in a case were non causal
 # dimentions are are used.
 def feature_relationships(numOfFeats):
 
@@ -97,7 +100,7 @@ def feature_relationships(numOfFeats):
 		num_feat.append(feats)
 		List_SSE.append(SSE)
 		List_SSEt.append(SSEt)
-		print X.shape
+		#print X.shape
 	return [num_feat, List_SSE, List_SSEt]
 
 # Plots the relationship between SSE and number of random features
@@ -123,12 +126,12 @@ def plot_lambda_relationship(lambda_list, List_SSE, List_SSEt):
 	plt.figure(1)
 	plt.subplot(211)
 	plt.ylabel("SSE")
-	plt.plot(num_feat, List_SSE, 'ro')
+	plt.plot(num_feat, List_SSE, 'r--')
 	plt.title("Training Data")
 	plt.subplot(212)
 	plt.xlabel("Value of lambda")
 	plt.ylabel("SSE")
-	plt.plot(num_feat, List_SSEt, 'bo')
+	plt.plot(num_feat, List_SSEt, 'b')
 	plt.title("Testing Data")
 	plt.show()
 	return 0
@@ -148,6 +151,9 @@ def weight_vector_variant(X,Y,lamb):
 
 	return W
 
+# Calculates the different SSEs for training and testing data
+# with lambda values set to all the numbers in the input parameter
+# list 'lambda_list'.
 def lambda_relationships(lambda_list):
 
 	[X,Y] = load_matrix("housing_train.txt")
@@ -168,7 +174,7 @@ def lambda_relationships(lambda_list):
 	return [lambda_list, List_SSE, List_SSEt]
 
 
-
+# Helper for pretty printing of numpy matrix objects
 def printMatrixE(a):
    print "Matrix["+("%d" %a.shape[0])+"]["+("%d" %a.shape[1])+"]"
    rows = a.shape[0]
@@ -203,8 +209,8 @@ def main():
     E = sum_sq_err(W,X,Y)
     Et = sum_sq_err(W,Xt,Yt)
     #print sum square error
-    print("Training Data With Dummy SSE: ", E)
-    print("Test Data With Dummy SSE: ", Et)
+    print "Training Data With Dummy SSE: ", E
+    print "Test Data With Dummy SSE: ", Et
 
 
     ### SSE witout dummy
@@ -218,26 +224,38 @@ def main():
     E_nd = sum_sq_err(W_nd,X_nd,Y)
     Et_nd = sum_sq_err(W_nd,Xt_nd,Yt)
     #print sum square error
-    print("Training Data Without Dummy SSE: ", E_nd)
-    print("Test Data Without Dummy SSE: ", Et_nd)
+    print "Training Data Without Dummy SSE: ", E_nd
+    print "Test Data Without Dummy SSE: ", Et_nd
 
+    print "\nThis Demonstrates that adding a dummy variable will produce"
+    print "result in overfitting. Dummy variable allows the regression to"
+    print "bend in alternative dimension.\n\n"
+
+    print "Now we will plot this effect by continually adding random"
+    print "variables and plotting the resulting training SSE and Test"
+    print "SSE aginst the number of random variables."
 
 
     ### adding features
     [num_feat, List_SSE, List_SSEt] = feature_relationships(10)
     plot_relationship(num_feat, List_SSE, List_SSEt,"Number of Additonal Features")
-    print num_feat
-    print List_SSE
-    print List_SSEt
+    #print num_feat
+    #print List_SSE
+    #print List_SSEt
 
+
+
+    print "\n\nNow we see the value of adding the hyper parameter Lambda"
     #### regression with lambda
-    lambda_list = [0.01, 0.05, 0.1, 0.5, 1, 5]
+    lambda_list = [0.01, 0.05, 0.1, 0.15, 0.25, 0.5, 1, 2, 5]
     [lambda_lsit, List_SSE, List_SSEt] = lambda_relationships(lambda_list)
     plot_relationship(lambda_list, List_SSE, List_SSEt,"Value of Lambda")
-    print lambda_list
-    print List_SSE
-    print List_SSEt
-
+    #print lambda_list
+    #print List_SSE
+    #print List_SSEt
+    print "\nWe can see that the training SSE continually increases with lambda but"
+    print "but the testing SSE decreases for a time before rising. This shows that"
+    print "the introduction of the Lambda hyperparameter can decrease overfitting"
     #printMatrixE(X_nd)
 
 
